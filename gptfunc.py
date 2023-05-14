e9cce27501cdf5c9a4a15c232c59398aaa35a004
Vincent Yang <yf2005@gmail.com>
2023-05-15 00:25:04 +0800

    update prompt to output json

--- gptfunc.py
+++ gptfunc.py
@@ -1,10 +1,11 @@
-from langchain.llms import OpenAIChat
+
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     AIMessage,
     HumanMessage,
     SystemMessage
 )
+
 from langchain import LLMChain
 from langchain.prompts.chat import (
     ChatPromptTemplate,
@@ -16,34 +17,63 @@ import os
 import json
 
 
-# read config from config.json
-with open("config.json", "r") as f:
-    config = json.load(f)
+from dotenv import load_dotenv
 
-# get openai api key from config.json
-api_key = config["openai_api_key"]
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = api_key
+AZURE_BASE_URL = os.environ.get("AZURE_BASE_URL")
+AZURE_API_KEY = os.environ.get("AZURE_API_KEY")
+AZURE_DEPLOYMENT_NAME = os.environ.get("AZURE_DEPLOYMENT_NAME")
 
+COHERE_API_KEY=os.environ.get("COHERE_API_KEY")
+
+import openai
+# openai.api_base="https://tiny-shadow-5144.vixt.workers.dev/v1"
+# openai.api_base="https://super-heart-4116.vixt.workers.dev/v1"
+openai.api_base="https://az.139105.xyz/v1"
+
+# llm = ChatOpenAI(model_name=model_name )
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=AZURE_API_KEY ) 
+                    
+
+# use azure model
+#     llm = AzureChatOpenAI(
+#     openai_api_base=AZURE_BASE_URL,
+#     openai_api_version="2023-03-15-preview",
+#     deployment_name=AZURE_DEPLOYMENT_NAME,
+#     openai_api_key=AZURE_API_KEY,
+#     openai_api_type = "azure",
+# )
+# use cohere model
+# llm = Cohere(model="command-xlarge-nightly",cohere_api_key=COHERE_API_KEY,temperature=0)
 
 
 def get_chatbot_response(text,model_name="gpt-3.5-turbo"):
     template="你是一位十分善于解决问题、按步骤思考的专业咨询顾问。"
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
-    human_template = "针对以下监管要求编写详细的审计步骤、访谈问题和资料清单：{text}"
+    human_template = f"""
+    针对监管要求编写以下详细内容：
+    - 审计步骤
+    - 访谈问题
+    - 资料清单
+    
+    监管要求使用'''进行间隔。
+    审计步骤的输出的格式为JSON，key为步骤名称，value为步骤内容。
+    访谈问题的输出的格式为JSON，key为问题名称，value为问题内容。
+    资料清单的输出的格式为JSON，key为资料名称，value为资料内容。
+    审计步骤的输出、访谈问题的输出、资料清单的输出之间使用'''进行间隔。
+
+    监管要求：'''{text}'''
+    """
+
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-#     messages = [
-#     SystemMessage(content= "你是一位十分善于解决问题、按步骤思考的专业咨询顾问。"),
-#     # combine the prompt and the instruction with the end-of-sequence token
-#     HumanMessage(content=  "针对以下监管要求编写详细的审计步骤："+prompt),
-# ]
     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
-    chat = ChatOpenAI(model_name=model_name)#, max_tokens=512)
+    # chat = ChatOpenAI(model_name=model_name)#, max_tokens=512)
 
-    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    chain = LLMChain(llm=llm, prompt=chat_prompt)
     # response = chat(chat_prompt.format_prompt(text=text).to_messages())
     response = chain.run(text=text)
     return response
