fc46c3ace6e85acfa0c2e3b3c5fe4bd1f81126f2
Vincent Yang <yf2005@gmail.com>
2023-05-24 21:15:31 +0800

    update prompt to format output

--- app.py
+++ app.py
@@ -1,62 +1,74 @@
 from textwrap import wrap
-import streamlit as st
-import pandas as pd
+
 import docx
 import numpy as np
+import pandas as pd
+import streamlit as st
+
 # from plc2audit import predict
-from gptfunc import get_chatbot_response
+from gptfunc import (
+    convert_json_to_df,
+    get_chatbot_response,
+    merge_items,
+    separate_items,
+)
+
 
 def main():
-
     # choose input method of manual or upload file
-    input_method = st.sidebar.radio('Input Method', ('Manual', 'Upload File'))
+    input_method = st.sidebar.radio("Input Method", ("Manual", "Upload File"))
     # choose model
-    model_name = st.sidebar.selectbox('选择模型', ['gpt-3.5-turbo', 'gpt-4'])
+    model_name = st.sidebar.selectbox("选择模型", ["gpt-3.5-turbo", "gpt-4"])
 
-    if input_method == 'Manual':
-        proc_text = st.text_area('Testing Procedures')
+    if input_method == "Manual":
+        proc_text = st.text_area("Testing Procedures")
 
-        if st.button('Predict'):
+        if st.button("Predict"):
             # get prediction result
-            result = get_chatbot_response(proc_text,model_name)
+            result = get_chatbot_response(proc_text, model_name)
+            # convert json to dataframe
+            df = convert_json_to_df(result)
             # display result
-            st.write(result)
+            st.table(df.astype(str))
 
-    elif input_method == 'Upload File':
+    elif input_method == "Upload File":
         # upload file
-        upload_file = st.file_uploader('Upload workpaper',
-                                       type=['xlsx', 'docx'])
+        upload_file = st.file_uploader("Upload workpaper", type=["xlsx", "docx"])
         if upload_file is not None:
             # if upload file is xlsx
-            if upload_file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            if (
+                upload_file.type
+                == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            ):
                 # get sheet names list from excel file
                 xls = pd.ExcelFile(upload_file)
                 sheets = xls.sheet_names
                 # choose sheet name and click button
-                sheet_name = st.selectbox('Choose sheetname', sheets)
+                sheet_name = st.selectbox("Choose sheetname", sheets)
 
                 # choose header row
-                header_row = st.number_input('Choose header row',
-                                             min_value=0,
-                                             max_value=10,
-                                             value=0)
-                df = pd.read_excel(upload_file,
-                                   header=header_row,
-                                   sheet_name=sheet_name)
+                header_row = st.number_input(
+                    "Choose header row", min_value=0, max_value=10, value=0
+                )
+                df = pd.read_excel(
+                    upload_file, header=header_row, sheet_name=sheet_name
+                )
                 # filllna
-                df = df.fillna('')
+                df = df.fillna("")
                 # display the first five rows
                 st.write(df.astype(str))
 
                 # get df columns
                 cols = df.columns
                 # choose proc_text and audit_text column
-                proc_col = st.sidebar.selectbox('Choose procedure column',
-                                                cols)
+                proc_col = st.sidebar.selectbox("Choose procedure column", cols)
                 # get proc_text and audit_text list
                 proc_list = df[proc_col].tolist()
 
-            elif upload_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            elif (
+                upload_file.type
+                == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            ):
                 # read docx file
                 document = docx.Document(upload_file)
                 # get table data
@@ -73,12 +85,11 @@ def main():
                 # get tablels index list
                 tablels_index = list(range(len(tablels)))
                 # choose tablels index
-                tablels_no = st.selectbox('Choose table index', tablels_index)
+                tablels_no = st.selectbox("Choose table index", tablels_index)
                 # choose header row
-                header_row = st.number_input('Choose header row',
-                                             min_value=0,
-                                             max_value=10,
-                                             value=0)
+                header_row = st.number_input(
+                    "Choose header row", min_value=0, max_value=10, value=0
+                )
 
                 if tablels_no is not None:
                     # get tablels
@@ -92,15 +103,14 @@ def main():
                     # get df columns
                     cols = df.columns
                     # choose proc_text and audit_text column
-                    proc_col = st.sidebar.selectbox('Choose procedure column',
-                                                    cols)
+                    proc_col = st.sidebar.selectbox("Choose procedure column", cols)
                     # get proc_text and audit_text list
                     proc_list = df[proc_col].tolist()
                 else:
-                    st.error('No table found in the document')
+                    st.error("No table found in the document")
                     proc_list = []
         else:
-            st.error('No file selected')
+            st.error("No file selected")
             proc_list = []
 
         # top = st.sidebar.slider('AuditProc Generation Number',
@@ -117,51 +127,51 @@ def main():
 
         # if proc_list or audit_list is empty or not equal
         if proc_len == 0:
-            st.error('Procedure list must not empty')
+            st.error("Procedure list must not empty")
             return
         else:
             # choose start and end index
-            start_idx = st.sidebar.number_input('Choose start index',
-                                                min_value=0,
-                                                max_value=proc_len - 1,
-                                                value=0)
-            end_idx = st.sidebar.number_input('Choose end index',
-                                            min_value=start_idx,
-                                            max_value=proc_len - 1,
-                                            value=proc_len - 1)
+            start_idx = st.sidebar.number_input(
+                "Choose start index", min_value=0, max_value=proc_len - 1, value=0
+            )
+            end_idx = st.sidebar.number_input(
+                "Choose end index",
+                min_value=start_idx,
+                max_value=proc_len - 1,
+                value=proc_len - 1,
+            )
 
             # get proc_list and audit_list
-            subproc_list = proc_list[start_idx:end_idx + 1]
+            subproc_list = proc_list[start_idx : end_idx + 1]
 
         # display subproc_list
-        st.subheader('Procedure List')
+        st.subheader("Procedure List")
         # display subproc_list
         for i, proc in enumerate(subproc_list):
-            st.markdown('##### ' + str(i) + ": " + proc)
+            st.markdown("##### " + str(i) + ": " + proc)
 
         # input prompt text
         # prompt_text = st.sidebar.text_area('Prompt Text')
 
-        search = st.sidebar.button('Convert')
+        search = st.sidebar.button("Convert")
 
         if search:
             # split list into batch of 5
             batch_num = 5
             proc_list_batch = [
-                subproc_list[i:i + batch_num]
+                subproc_list[i : i + batch_num]
                 for i in range(0, len(subproc_list), batch_num)
             ]
 
             dfls = []
             # get proc and audit batch
             for j, proc_batch in enumerate(proc_list_batch):
-
-                with st.spinner('Converting...'):
+                with st.spinner("Converting..."):
                     # range of the batch
                     start = j * batch_num + 1
                     end = start + len(proc_batch) - 1
 
-                    st.subheader('Results: ' + f'{start}-{end}')
+                    st.subheader("Results: " + f"{start}-{end}")
 
                     # get audit list
                     # audit_batch = predict(proc_batch,8, top, max_length)
@@ -173,43 +183,61 @@ def main():
                         # get audit list
                         # audit_list = audit_batch[audit_start:audit_end]
                         # check if proc is blank, after remove space
-                        if proc.replace(' ', '') == '':
-                            st.error('Procedure must not empty')
-                            audit_list = ''
+                        if proc.replace(" ", "") == "":
+                            st.error("Procedure must not empty")
+                            audit_list = ""
+                            merged_audit_list = ""
                         else:
-                            audit_list = get_chatbot_response(proc,model_name)
-                        auditls.append(audit_list)
+                            audit_list = get_chatbot_response(proc, model_name)
+                            # merge audit list
+                            merged_audit_list = merge_items(audit_list)
+                        auditls.append(merged_audit_list)
                         # get count number from start
                         count = str(j * batch_num + i + 1)
-                        
+
                         # print proc and audit list
-                        st.warning('Policy '+count+':')
+                        st.warning("Policy " + count + ":")
                         st.write(proc)
                         # print audit list
-                        st.info('Audit Procedure '+count+': ')
+                        st.info("Audit Procedure " + count + ": ")
                         # for audit in audit_list:
                         #     st.write(audit)
-                        st.write(audit_list)
+                        # convert to df
+                        df = convert_json_to_df(audit_list)
+                        st.table(df)
+                        st.write(merged_audit_list)
+
+                    (
+                        audit_work_list,
+                        interview_question_list,
+                        document_list,
+                    ) = separate_items(auditls)
                     # convert to dataframe
-                    df = pd.DataFrame({
-                        'policy': proc_batch,
-                        'auditproc': auditls
-                    })
+                    df = pd.DataFrame(
+                        {
+                            "policy": proc_batch,
+                            "auditwork": audit_work_list,
+                            "interviewquestion": interview_question_list,
+                            "document": document_list,
+                        }
+                    )
                     dfls.append(df)
 
             # conversion is done
-            st.sidebar.success('Conversion Finish')
+            st.sidebar.success("Conversion Finish")
             # if dfls not empty
             if dfls:
                 alldf = pd.concat(dfls)
                 # df explode by auditproc and reset index
-                alldf = alldf.explode('auditproc')
-                alldf = alldf.reset_index(drop=True)                    
-                st.sidebar.download_button(label='Download',
-                                        data=alldf.to_csv(),
-                                        file_name='plc2auditresult.csv',
-                                        mime='text/csv')
+                # alldf = alldf.explode('auditproc')
+                # alldf = alldf.reset_index(drop=True)
+                st.sidebar.download_button(
+                    label="Download",
+                    data=alldf.to_csv(),
+                    file_name="plc2auditresult.csv",
+                    mime="text/csv",
+                )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- gptfunc.py
+++ gptfunc.py
@@ -1,23 +1,16 @@
-import pandas as pd
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+import json
+import os
 
+import pandas as pd
+from dotenv import load_dotenv
 from langchain import LLMChain
+from langchain.chat_models import AzureChatOpenAI, ChatOpenAI
 from langchain.prompts.chat import (
     ChatPromptTemplate,
-    SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
 )
-
-import os
-import json
-
-
-from dotenv import load_dotenv
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
 load_dotenv()
 
@@ -25,19 +18,20 @@ AZURE_BASE_URL = os.environ.get("AZURE_BASE_URL")
 AZURE_API_KEY = os.environ.get("AZURE_API_KEY")
 AZURE_DEPLOYMENT_NAME = os.environ.get("AZURE_DEPLOYMENT_NAME")
 
-COHERE_API_KEY=os.environ.get("COHERE_API_KEY")
+COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
 
 import openai
+
 # openai.api_base="https://tiny-shadow-5144.vixt.workers.dev/v1"
 # openai.api_base="https://super-heart-4116.vixt.workers.dev/v1"
-openai.api_base="https://az.139105.xyz/v1"
+openai.api_base = "https://az.139105.xyz/v1"
 
 # llm = ChatOpenAI(model_name=model_name )
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=AZURE_API_KEY ) 
-                    
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=AZURE_API_KEY)
+
 
 # use azure model
-#     llm = AzureChatOpenAI(
+# llm = AzureChatOpenAI(
 #     openai_api_base=AZURE_BASE_URL,
 #     openai_api_version="2023-03-15-preview",
 #     deployment_name=AZURE_DEPLOYMENT_NAME,
@@ -48,8 +42,8 @@ llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=AZURE_API_KEY )
 # llm = Cohere(model="command-xlarge-nightly",cohere_api_key=COHERE_API_KEY,temperature=0)
 
 
-def get_chatbot_response(text,model_name="gpt-3.5-turbo"):
-    template="你是一位十分善于解决问题、按步骤思考的专业咨询顾问。"
+def get_chatbot_response(text, model_name="gpt-3.5-turbo"):
+    template = "你是一位十分善于解决问题、按步骤思考的专业咨询顾问。"
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
     human_template = f"""
@@ -60,21 +54,23 @@ def get_chatbot_response(text,model_name="gpt-3.5-turbo"):
     - '资料清单'：'针对每一项审计工作，需要准备的审计资料'
 
     监管要求使用'''进行间隔。
-    
-    输出的格式为JSON，每一个审计步骤应该是一个独立的对象，包含以下四个字段："步骤编号"，"审计工作"，"访谈问题"，"资料清单"。字段的值应该是具体的信息。
 
-    包含一个对象的输出样例：
-    {{{{"步骤编号": "1",
+    输出的格式应为JSON列表，每个列表项为一个对象，包含以上四个字段："步骤编号"，"审计工作"，"访谈问题"，"资料清单"。字段的值应为具体的信息。
+
+    以下是一个包含一个对象的输出样例：
+    [{{{{"步骤编号": "1",
     "审计工作": "确认证券期货机构是否有相关文档分类管理制度",
     "访谈问题": "请描述证券期货机构的文档分类管理制度",
-    "资料清单": "证券期货机构的文档分类管理制度" }}}}
+    "资料清单": "证券期货机构的文档分类管理制度" }}}}]
 
     监管要求：'''{text}'''
     """
 
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
 
     # chat = ChatOpenAI(model_name=model_name)#, max_tokens=512)
 
@@ -89,7 +85,6 @@ def get_chatbot_response(text,model_name="gpt-3.5-turbo"):
 
 # convert json response to dataframe
 def convert_json_to_df(response):
-    
     data_dict = json.loads(response)
 
     # Convert dictionary to DataFrame
@@ -97,3 +92,30 @@ def convert_json_to_df(response):
     df = pd.DataFrame.from_dict(data_dict)
 
     return df
+
+
+def merge_items(data):
+    data_dict = json.loads(data)
+    merged = {"审计工作": [], "访谈问题": [], "资料清单": []}
+
+    for item in data_dict:
+        for key in merged:
+            merged[key].append(item["步骤编号"] + "." + item[key])
+
+    for key in merged:
+        merged[key] = ";".join(merged[key])
+
+    return merged
+
+
+def separate_items(input_list):
+    audit_work_list = []
+    interview_question_list = []
+    document_list = []
+
+    for item in input_list:
+        audit_work_list.append(item["审计工作"])
+        interview_question_list.append(item["访谈问题"])
+        document_list.append(item["资料清单"])
+
+    return audit_work_list, interview_question_list, document_list
